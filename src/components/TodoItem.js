37afddadf0d51dbfc66ef65b41d0f107bf852257
ChoSangok <chod1510@naver.com>
2024-10-19 00:12:05 +0900

    task_finish

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import api from "../utils/api";
 
-const TodoItem = () => {
+const TodoItem = ({ item, getTask }) => {
+  const hasDelete = async () => {
+    await api.delete(`/tasks/${item._id}`);
+    getTask();
+  };
+  const hasIsComplete = async () => {
+    await api.put(`/tasks/${item._id}`, { isComplete: !item.isComplete });
+    getTask();
+  };
+
   return (
     <Row>
       <Col xs={12}>
-        <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
-
+        <div className={`todo-item ${item.isComplete ? "finish" : ""}`}>
+          <div className="todo-content">{item.task}</div>
           <div>
-            <button className="button-delete">삭제</button>
-            <button className="button-delete">끝남</button>
+            <button className="button-delete" onClick={() => hasDelete()}>
+              삭제
+            </button>
+            <button className="button-delete" onClick={() => hasIsComplete()}>
+              {item.isComplete ? "끝내지 못했다" : "끝!!!"}
+            </button>
           </div>
         </div>
       </Col>
