cfdc5647df227438d0a9b87efe58ca876faa828e
ChoSangok <chod1510@naver.com>
2024-10-19 07:19:42 +0900

    taskAdd

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,31 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import api from "../utils/api";
 
 const TodoItem = ({ item, getTask }) => {
-  const hasDelete = async () => {
+  const [revise, setRevise] = useState(false);
+  const [task, setTask] = useState("");
+
+  const handleDelete = async () => {
     await api.delete(`/tasks/${item._id}`);
     getTask();
   };
-  const hasIsComplete = async () => {
+  const handleIsComplete = async () => {
     await api.put(`/tasks/${item._id}`, { isComplete: !item.isComplete });
+    getTask();
+  };
+  const handleTask = async (task) => {
+    await api.put(`/tasks/${item._id}`, { task: task });
+    setRevise(!revise);
     getTask();
   };
 
   return (
     <Row>
       <Col xs={12}>
-        <div className={`todo-item ${item.isComplete ? "finish" : ""}`}>
-          <div className="todo-content">{item.task}</div>
-          <div>
-            <button className="button-delete" onClick={() => hasDelete()}>
-              삭제
-            </button>
-            <button className="button-delete" onClick={() => hasIsComplete()}>
-              {item.isComplete ? "끝내지 못했다" : "끝!!!"}
-            </button>
+        {revise ? (
+          <div className={`todo-item ${item.isComplete ? "finish" : ""}`}>
+            <input
+              className="todo-content"
+              type="text"
+              value={task}
+              onChange={(e) => {
+                setTask(e.target.value);
+              }}
+              autoFocus
+            ></input>
+
+            <div>
+              <button
+                className="button-delete"
+                onClick={() => {
+                  handleTask(task);
+                }}
+              >
+                완료
+              </button>
+              <button
+                className="button-delete"
+                onClick={() => setRevise(!revise)}
+              >
+                취소
+              </button>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className={`todo-item ${item.isComplete ? "finish" : ""}`}>
+            <div className="todo-content">{item.task}</div>
+            <div>
+              <button
+                className="button-delete"
+                onClick={() => setRevise(!revise)}
+              >
+                수정
+              </button>
+              <button className="button-delete" onClick={() => handleDelete()}>
+                삭제
+              </button>
+              <button
+                className="button-delete"
+                onClick={() => handleIsComplete()}
+              >
+                {item.isComplete ? "끝내지 못했다" : "끝!!!"}
+              </button>
+            </div>
+          </div>
+        )}
       </Col>
     </Row>
   );
