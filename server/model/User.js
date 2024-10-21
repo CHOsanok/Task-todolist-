33af16af1eeafc360351ee15cad0d84abc5b59f7
ChoSangok <chod1510@naver.com>
2024-10-21 10:36:18 +0900

    user주석처리

--- server/model/User.js
+++ server/model/User.js
@@ -1,29 +1,29 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-var jwt = require("jsonwebtoken");
-require("dotenv").config();
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+// var jwt = require("jsonwebtoken");
+// require("dotenv").config();
+// const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
-const userSchema = Schema(
-  {
-    name: { type: String, require: true },
-    email: { type: String, require: true },
-    password: { type: String, require: true },
-  },
-  { timestamps: true }
-);
+// const userSchema = Schema(
+//   {
+//     name: { type: String, require: true },
+//     email: { type: String, require: true },
+//     password: { type: String, require: true },
+//   },
+//   { timestamps: true }
+// );
 
-userSchema.methods.generateToken = function () {
-  const token = jwt.sign({ _id: this.id }, JWT_SECRET_KEY, { expiresIn: "1d" });
+// userSchema.methods.generateToken = function () {
+//   const token = jwt.sign({ _id: this.id }, JWT_SECRET_KEY, { expiresIn: "1d" });
 
-  return token;
-};
-userSchema.methods.toJSON = function () {
-  const { __v, password, ...obj } = this._doc;
+//   return token;
+// };
+// userSchema.methods.toJSON = function () {
+//   const { __v, password, ...obj } = this._doc;
 
-  return obj;
-};
+//   return obj;
+// };
 
-const User = mongoose.model("User", userSchema);
+// const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+// module.exports = User;
--- server/routes/taks.api.js
+++ server/routes/taks.api.js
@@ -3,11 +3,8 @@ const taskController = require("../controller/task.controller");
 const router = express.Router();
 
 router.post("/", taskController.createTask);
-
 router.get("/", taskController.getTask);
-
 router.put("/:id", taskController.updateTask);
-
 router.delete("/:id", taskController.deleteTak);
 
 module.exports = router;
