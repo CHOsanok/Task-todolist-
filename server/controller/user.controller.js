7fb19d59af06b87f0f0a8e1e2e94d4f22106769f
ChoSangok <chod1510@naver.com>
2024-10-22 00:44:48 +0900

    error수정

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -29,17 +29,18 @@ userController.loginWithEmail = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email }, "-createdAt -updatedAt");
-    const isMatch = bcrypt.compareSync(password, user.password);
+    if (user) {
+      const isMatch = bcrypt.compareSync(password, user.password);
 
-    if (isMatch) {
-      const token = user.generateToken();
+      if (isMatch) {
+        const token = user.generateToken();
 
-      return res.status(200).json({ status: "success", user, token });
+        return res.status(200).json({ status: "success", user, token });
+      }
     }
-
     throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
-  } catch (err) {
-    res.status(400).json({ status: "fail", error: err });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
   }
 };
 
