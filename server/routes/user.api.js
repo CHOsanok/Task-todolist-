02972a9acd0b9e8fa6f8dd56803486c11bcb6d19
ChoSangok <chod1510@naver.com>
2024-10-23 14:01:20 +0900

    addGetUserController

--- server/routes/user.api.js
+++ server/routes/user.api.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controller/user.controller");
+const authController = require("../controller/auth.controller");
 
 router.post("/", userController.creatUser);
 router.post("/login", userController.loginWithEmail);
-
+router.get("/me", authController.authenticate, userController.getUser);
 module.exports = router;
